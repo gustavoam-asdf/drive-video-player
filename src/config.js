c5614cbc4d2f8ca07ef324348465b3be59e51904
Gustavo A <gustavo.atencio@outlook.com>
2021-12-29 00:32:25 -0500

    Adding dotenv file

--- src/config.js
+++ src/config.js
@@ -1,2 +1,7 @@
-// Your enviromental variables configurated, in this case with vite:
-// https://vitejs.dev/guide/env-and-mode.html#env-variables-and-modes
+export const VITE_DRIVE_CLIENT_ID = import.meta.env.VITE_DRIVE_CLIENT_ID
+export const VITE_DRIVE_CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET
+
+export default {
+  VITE_DRIVE_CLIENT_SECRET,
+  VITE_DRIVE_CLIENT_ID
+}
